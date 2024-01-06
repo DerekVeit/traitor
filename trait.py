e2e0d69fe56333c93c4c9c6c404ef98246b2884e
Derek Veit <derek.veit>
2024-01-06 05:20:30 -0800

    Initial trait.py.

---
+++ trait.py
@@ -0,0 +1,59 @@
+
+from dataclasses import dataclass
+
+def Traitable(subject):
+    subject._traits = []
+    subject.__getattr__ = employ_traits
+    return subject
+
+
+def employ_traits(obj, attr):
+    for trait in obj._traits:
+        if attr in trait.__dict__:
+            def method(*args, **kwargs):
+                return getattr(trait, attr)(obj, *args, **kwargs)
+            return method
+    raise AttributeError('%r object has no attribute %r' %
+                         (type(obj).__name__, attr))
+
+
+@Traitable
+@dataclass
+class Strings:
+    strings: list[str]
+
+
+@Traitable
+@dataclass
+class Numbers:
+    nums: list[int]
+
+
+def Trait(subject):
+    def wrapper(trait):
+        subject._traits.append(trait)
+        return trait
+    return wrapper
+
+
+@Trait(Strings)
+class Arrangement:
+    def sorted(self):
+        return sorted(self.strings)
+
+
+@Trait(Numbers)
+class Arrangement:
+    def sorted(self):
+        return sorted(self.nums)
+
+
+
+def main():
+    lines = Strings('Ben Derek Carl Abe'.split())
+    for line in lines.sorted():
+        print(line)
+    scores = Numbers([90, 84, 92, 87])
+    for number in scores.sorted():
+        print(number)
+
