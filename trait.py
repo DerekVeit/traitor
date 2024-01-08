407d9a17c5e8a95c48346553b3fa510f682ab921
Derek Veit <derek.veit>
2024-01-08 04:39:37 -0800

    Raise AttributeError for ambiguous trait method calls.

--- trait.py
+++ trait.py
@@ -14,11 +14,18 @@ impl_for.traits = {}
 
 
 def employ_traits(obj, attr):
-    for trait in obj._traits.values():
+    traits = []
+    for trait_name, trait in obj._traits.items():
         if attr in trait.__dict__:
-            def method(*args, **kwargs):
-                return getattr(trait, attr)(obj, *args, **kwargs)
-            return method
+            traits.append((trait_name, trait))
+    if len(traits) == 1:
+        trait_name, trait = traits[0]
+        def method(*args, **kwargs):
+            return getattr(trait, attr)(obj, *args, **kwargs)
+        return method
+    elif len(traits) > 1:
+        raise AttributeError('%r object has multiple traits defining attribute %r' %
+                             (type(obj).__name__, attr))
     raise AttributeError('%r object has no attribute %r' %
                          (type(obj).__name__, attr))
 
