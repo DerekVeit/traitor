eb57681e7ebeef07f5bf4b513606fd1f3491fe6f
Derek Veit <derek.veit>
2024-01-08 05:13:10 -0800

    Allow explicitly specifying a trait for a method call.

--- trait.py
+++ trait.py
@@ -6,6 +6,7 @@ def impl_for(subject):
         subject._traits = {}
         subject.__getattr__ = employ_traits
     def wrapper(trait):
+        trait._objects = []
         subject._traits[trait.__name__] = trait
         return impl_for.traits[trait.__name__]
     return wrapper
@@ -14,10 +15,18 @@ impl_for.traits = {}
 
 
 def employ_traits(obj, attr):
+    if attr in obj._traits:
+        trait = obj._traits[attr]
+        trait._objects.append(obj)
+        return obj
     traits = []
     for trait_name, trait in obj._traits.items():
         if attr in trait.__dict__:
             traits.append((trait_name, trait))
+            if any(id(item) == id(obj) for item in trait._objects):
+                trait._objects = [item for item in trait._objects if id(item) != id(obj)]
+                traits = traits[-1:]
+                break
     if len(traits) == 1:
         trait_name, trait = traits[0]
         def method(*args, **kwargs):
@@ -53,6 +62,14 @@ class Arrangement:
         """
 
 
+@trait
+class Reversal:
+    def sorted(self):
+        """
+        returns a reversed sorted list
+        """
+
+
 @impl_for(Strings)
 class Arrangement:
     def sorted(self):
@@ -66,11 +83,20 @@ class Arrangement:
 
 
 
+@impl_for(Numbers)
+class Reversal:
+    def sorted(self):
+        return reversed(sorted(self.nums))
+
+
+
 def main():
     lines = Strings('Ben Derek Carl Abe'.split())
     for line in lines.sorted():
         print(line)
     scores = Numbers([90, 84, 92, 87])
+    for number in scores.Arrangement.sorted():
+        print(number)
     for number in scores.sorted():
         print(number)
 
